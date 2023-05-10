b3f76f5fefb3be79a76b691addbf92e30d1ca864
jaeshim <jaeshim@gmarket.com>
2023-05-11 08:25:11 +0900

    initial commit

---
+++ inflearn/src/main/java/com/jaeshim/inflearn/spring/mvc/thymeleaf/basic/Application.java
@@ -0,0 +1,13 @@
+package com.jaeshim.inflearn.spring.mvc.thymeleaf.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
